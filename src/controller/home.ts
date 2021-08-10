e577df02a4d3f36b87bb6d5da234882bedc63686
kaindy7633 <kaindy7633@gmail.com>
2021-08-10 22:54:14 +0800

    设置响应头

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Provide } from '@midwayjs/decorator';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Provide,
+  SetHeader,
+} from '@midwayjs/decorator';
 
 @Provide()
 @Controller('/')
@@ -11,5 +18,19 @@ export class HomeController {
   @Post('/update')
   async updateData() {
     return 'This is a post method';
+  }
+
+  // 设置HttpCode
+  @Get('/http-code')
+  @HttpCode(201)
+  async showHttpCode() {
+    return 'Hello HTTP Code';
+  }
+
+  // 简单设置Header
+  @Get('/header')
+  @SetHeader('x-bbb', '123')
+  async setHeader() {
+    return 'Hello Set Header';
   }
 }
